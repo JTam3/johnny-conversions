48514938fdd100563c4a7cab7c3627d411af138c
Mostafa Lotfi <mostycool@gmail.com>
2016-10-14 10:28:08 -0400

    added gallon to liter

--- conversions.py
+++ conversions.py
@@ -1,10 +1,14 @@
-> > # My Conversion Tools
-> >
-> > def dollars2cents(dollars):
-> >     cents = dollars * 100
-> >     return cents
+# My Conversion Tools
+
+def dollars2cents(dollars):
+	cents = dollars * 100
+	return cents
 
 #add hours to minutes
 def hour2minutes(hours):
 	minutes = hours*60
 	return minutes
+
+#add gallons to liters
+def gallon2liter(gallon):
+	liter=gallon * 3.7895
